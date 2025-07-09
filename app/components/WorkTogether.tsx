d1d27db61e60ebdcabf53c9ff2bbefb0e4dbf7ec
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2025-07-09 23:24:32 +0300

    add work together section

---
+++ app/components/WorkTogether.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import Section from "./helper/Section";
+import Title from "./helper/Title";
+import Image from "next/image";
+import Text from "./helper/Text";
+import Button from "./helper/Button";
+import { GoArrowRight } from "react-icons/go";
+
+const WorkTogether = () => {
+  return (
+    <Section>
+      <div className="relative">
+        <Title extraStyles="relative z-[3]">Work together</Title>
+        <Image
+          src={"/vector.png"}
+          alt="line"
+          width={164}
+          height={26}
+          className="absolute bottom-0.5 right-[5px] "
+        />
+      </div>
+      <Text>
+        With whitepace, share your notes with your colleagues and collaborate on
+        them. You can also publish a note to the internet and share the URL with
+        others.
+      </Text>
+      <Button>
+        Try it now <GoArrowRight />
+      </Button>
+      <Image
+        src="/WTI.png"
+        alt="work together"
+        width={279}
+        height={260}
+        className="mt-[60px] mb-20"
+      />
+    </Section>
+  );
+};
+
+export default WorkTogether;
