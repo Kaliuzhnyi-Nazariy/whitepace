249bc4c0a7a454a723df0a7cf7d39efecaead7c2
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2025-07-11 22:33:31 +0300

    make children component responsive

--- app/components/helper/Section.tsx
+++ app/components/helper/Section.tsx
@@ -11,7 +11,9 @@ const Section = ({
     <section
       className={`${extraStyles && extraStyles} flex flex-col items-center`}
     >
-      <div className="w-[288px] mx-4 ">{children}</div>
+      <div className="w-[288px] md:w-[704px] mx-4 md:mx-5 [@media(min-width:1152px)]:w-[1088px] ">
+        {children}
+      </div>
       {/* <div className="px-4">{children}</div> */}
     </section>
   );
