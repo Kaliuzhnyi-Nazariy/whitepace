17eca255306072f3d792663a4ce1ad1a3fec3c13
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2025-07-11 22:32:33 +0300

    add changable margin top for text component

--- app/components/helper/Text.tsx
+++ app/components/helper/Text.tsx
@@ -3,15 +3,17 @@ import React from "react";
 const Text = ({
   children,
   extraStyles,
+  newMT,
 }: {
   children: React.ReactNode;
   extraStyles?: string;
+  newMT?: string;
 }) => {
   return (
     <p
-      className={` text-[18px] mt-6 ${
+      className={` text-[18px] ${
         extraStyles ? extraStyles : "font-light text-center"
-      }`}
+      } ${newMT ? newMT : "mt-6"}`}
     >
       {children}
     </p>
