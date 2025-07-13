5535d009d5ce7a2f374c6571dc038fdcd285c87c
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2025-07-13 23:37:45 +0300

    add one more field for adding styles

--- app/components/helper/Section.tsx
+++ app/components/helper/Section.tsx
@@ -3,15 +3,19 @@ import React from "react";
 const Section = ({
   children,
   extraStyles = "",
+  flexStyle = "",
 }: {
   children: React.ReactNode;
   extraStyles?: string;
+  flexStyle?: string;
 }) => {
   return (
     <section
       className={`${extraStyles && extraStyles} flex flex-col items-center`}
     >
-      <div className="w-[288px] md:w-[704px] mx-4 md:mx-5 [@media(min-width:1152px)]:w-[1088px] ">
+      <div
+        className={`max-md:w-[288px] max-[1151px]:w-[704px] mx-4 max-[1152px]:mx-5 max-[1440px]:mx-8 min-[1152px]:w-[1088px] min-[1440px]:w-[1376px] min-[1920px]:w-[1480px] ${flexStyle}`}
+      >
         {children}
       </div>
       {/* <div className="px-4">{children}</div> */}
