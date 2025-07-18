0c784a8ffb21b1da26e1c49f0cd91b4171e34e01
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2025-07-18 21:22:46 +0300

    layout styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.variable} antialiased overflow-x-hidden`}>
+      <body
+        className={`${inter.variable} antialiased overflow-x-hidden
+        `}
+      >
         {children}
       </body>
     </html>
